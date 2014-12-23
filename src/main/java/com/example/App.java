02d2578c8955728069323d6fbc20b4b66d817846
WizCollect <wizcollect@gmail.com>
2014-12-23 18:46:08 +0900

    Added pagination

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -18,7 +21,15 @@ public class App implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
         System.out.println(created + " is created!");
-        customerRepository.findAllOrderByName().forEach(System.out::println);
+
+        Pageable pageable = new PageRequest(0, 3);
+        Page<Customer> page = customerRepository.findAllOrderByName(pageable);
+
+        System.out.println("1ページあたりのデータ数=" + page.getSize());
+        System.out.println("現在のページ=" + page.getNumber());
+        System.out.println("全ページ数" + page.getTotalPages());
+        System.out.println("全データ数" + page.getTotalElements());
+        page.getContent().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
