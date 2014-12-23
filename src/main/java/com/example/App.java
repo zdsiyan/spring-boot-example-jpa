721063174ef3fdef91775dff145440e8ebbd54b3
WizCollect <wizcollect@gmail.com>
2014-12-23 18:36:15 +0900

    Using JPQL

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
         System.out.println(created + " is created!");
-        customerRepository.findAll().forEach(System.out::println);
+        customerRepository.findAllOrderByName().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
